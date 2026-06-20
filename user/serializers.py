74616f55b7d89545b93fc242e55e663cc4562797
Oleh M <oleh.murachov28@gmail.com>
2026-06-20 21:33:55 +0200

    popopop

--- user/serializers.py
+++ user/serializers.py
@@ -3,25 +3,13 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True,
-        min_length=5,
-        style={"input_type": "password"},
-    )
-
     class Meta:
         model = get_user_model()
-        fields = (
-            "id",
-            "username",
-            "password",
-            "email",
-            "is_staff",
-        )
-        read_only_fields = (
-            "id",
-            "is_staff",
-        )
+        fields = ("id", "username", "password", "email", "is_staff")
+        read_only_fields = ("id", "is_staff")
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(
